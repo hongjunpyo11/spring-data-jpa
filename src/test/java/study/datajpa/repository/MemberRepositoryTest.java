9dac2562333976a27e48e3bfe170c78722c2816d
hongjunpyo11 <jpyo98@naver.com>
2023-11-06 20:53:37 +0900

    반환 타입

--- src/test/java/study/datajpa/repository/MemberRepositoryTest.java
+++ src/test/java/study/datajpa/repository/MemberRepositoryTest.java
@@ -145,6 +145,16 @@ class MemberRepositoryTest {
         for (Member member : result) {
             System.out.println("member = " + member);
         }
+    }
 
+    @Test
+    public void returnType() {
+        Member m1 = new Member("AAA", 10);
+        Member m2 = new Member("AAA", 20);
+        memberRepository.save(m1);
+        memberRepository.save(m2);
+
+        Optional<Member> findMember = memberRepository.findOptionalByUsername("fasfwfqwfqwf");
+        System.out.println("findMember = " + findMember);
     }
 }
